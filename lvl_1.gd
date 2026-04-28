0ce047f48485cc300e49e8479e248b265bb40af1
Mario-Alcaraz <alcarazcorbachomario@elpuig.xeill.net>
2026-04-28 11:00:11 +0200

    28-04-2026

--- lvl_1.gd
+++ lvl_1.gd
@@ -4,7 +4,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		get_tree().change_scene_to_file("res://lvl_2.tscn")
 
-# Funciones de los pinchos
 func _on_area_2d_2_body_entered(body: Node2D): quitar_vida(body)
 func _on_area_2d_3_body_entered(body: Node2D): quitar_vida(body)
 func _on_area_2d_4_body_entered(body: Node2D): quitar_vida(body)
