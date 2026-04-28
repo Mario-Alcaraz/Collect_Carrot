eab575cf7a3876bfdbaa8777a9eaa7a7d05e5aae
Mario-Alcaraz <alcarazcorbachomario@elpuig.xeill.net>
2026-04-28 16:19:00 +0200

    28-Abril-2026

--- Global.gd
+++ Global.gd
@@ -1,7 +1,15 @@
 extends Node
 
 var contador_zanahorias : int = 0
-var salud_actual : int = 6  # 6 equivale a 3 corazones llenos (2 puntos por corazón)
+var salud_actual : int = 6: # 6 equivale a 3 corazones llenos
+	set(valor):
+		# Si el nuevo valor es menor que el actual, el conejo ha recibido daño
+		if valor < salud_actual:
+			var player = get_tree().get_first_node_in_group("player")
+			if player and player.has_method("recibir_daño"):
+				player.recibir_daño()
+		
+		salud_actual = valor
 
 func reiniciar_partida():
 	contador_zanahorias = 0
