1b6cb8a2b600aa0e361668b426c7291ffeb7ec15
Mario-Alcaraz <alcarazcorbachomario@elpuig.xeill.net>
2026-04-29 10:57:29 +0200

    29-04-2026

--- 3_pinchos_grandes.gd
+++ 3_pinchos_grandes.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
-func _on_area_2d_body_entered(_body: Node2D) -> void:
-	
-	get_tree().change_scene_to_file("res://menu_muerte.tscn")
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	# AÑADIDO: Verificación de grupo y resta de vida
+	if body.is_in_group("player"):
+		Global.salud_actual -= 1
+		
+		# Solo cambia de escena si el Global detecta que la vida llegó a 0
+		# (Esto ya lo hace el script del Player, así que no hace falta ponerlo aquí)
--- Global.gd
+++ Global.gd
@@ -3,7 +3,7 @@ extends Node
 var contador_zanahorias : int = 0
 var salud_actual : int = 6: # 6 equivale a 3 corazones llenos
 	set(valor):
-		# Si el nuevo valor es menor que el actual, el conejo ha recibido daño
+		# Si el nuevo valor es menor, activamos la reacción del player
 		if valor < salud_actual:
 			var player = get_tree().get_first_node_in_group("player")
 			if player and player.has_method("recibir_daño"):
