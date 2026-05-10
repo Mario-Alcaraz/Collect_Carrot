eeb19e0657fd9c7dd944e2f30dd6faa66a07dc55
Mario-Alcaraz <alcarazcorbachomario@elpuig.xeill.net>
2026-05-10 03:20:38 +0200

    10/05/2026

--- lvl_6.gd
+++ lvl_6.gd
@@ -3,7 +3,7 @@ extends Node2D
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		# Vuelve al inicio o pantalla final
-		get_tree().change_scene_to_file("res://lvl_1.tscn")
+		get_tree().change_scene_to_file("res://lvl_7.tscn")
 
 func _on_area_2d_2_body_entered(body): quitar_vida(body)
 
