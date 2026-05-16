6073dc492c3147a1037811e6f4ebde9c34544bc9
JonEL1010 <jbornas@elpuig.xeill.net>
2026-05-16 18:21:28 +0200

    16-05-2026

--- menu_pausa.gd
+++ menu_pausa.gd
@@ -1,48 +1,46 @@
 extends CanvasLayer
 
-@onready var continuar = $TextureRect/VBoxContainer/Continuar
-@onready var reiniciar = $TextureRect/VBoxContainer/Reiniciar
-@onready var salir = $TextureRect/VBoxContainer/SalirMenu
+@onready var Menu = $Menu
+@onready var boton_continuar = $TextureRect/VBoxContainer/Continuar
+@onready var boton_salir = $TextureRect/VBoxContainer/SalirMenu
+@onready var boton_reiniciar = $TextureRect/VBoxContainer/Reiniciar
 
 func _ready():
-	# IMPORTANTE: Esto permite que el menú funcione aunque el juego esté pausado
-	process_mode = Node.PROCESS_MODE_ALWAYS
-
-#Empezar totalmente oculto
-	hide()
-	get_tree().paused = false
-
-#Conexiones de botones
-	continuar.pressed.connect(_on_continuar_pressed)
-	reiniciar.pressed.connect(_on_reiniciar_pressed)
-	salir.pressed.connect(_on_salir_pressed)
+	process_mode = Node.PROCESS_MODE_ALWAYS 
+	layer = 128  
+	hide() 
+	
+	boton_continuar.pressed.connect(_on_continuar_pressed)
+	boton_salir.pressed.connect(_on_salir_pressed)
+	boton_reiniciar.pressed.connect(_on_reiniciar_pressed)
 
 func _unhandled_input(event):
-	# Si pulsas la tecla de pausa
 	if event.is_action_pressed("Pausa"):
-		# Si estamos en el menú principal, NO HACER NADA
-		# Asegúrate de que el nodo raíz de tu menú se llame "MenuPrincipal"
 		if get_tree().current_scene.name == "MenuPrincipal":
 			return
-
 		alternar_pausa()
 
 func alternar_pausa():
-	# Invertir estado de pausa
-	var nuevo_estado = !get_tree().paused
-	get_tree().paused = nuevo_estado
-	visible = nuevo_estado
+	get_tree().paused = !get_tree().paused
+	visible = get_tree().paused
+	
+	if visible:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		Menu.play()
+	else:
+		Menu.stop()
 
 func _on_continuar_pressed():
 	alternar_pausa()
 
 func _on_reiniciar_pressed():
+	Menu.stop()
 	get_tree().paused = false
 	hide()
 	get_tree().reload_current_scene()
-
+	
 func _on_salir_pressed():
+	Menu.stop()
 	get_tree().paused = false
 	hide()
-	# Cambia esto a la ruta REAL de tu archivo
 	get_tree().change_scene_to_file("res://menu_principal.tscn")
